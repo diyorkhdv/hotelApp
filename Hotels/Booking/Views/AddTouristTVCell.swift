8e1f71616cfd69e6b96abcf420c0fd919ba22e21
diyorkhdv <diyor.khalmukhamedov@gmail.com>
2024-01-20 04:03:23 +0500

    adding tourist feature updated

--- Hotels/Booking/Views/AddTouristTVCell.swift
+++ Hotels/Booking/Views/AddTouristTVCell.swift
@@ -2,7 +2,81 @@
 //  AddTouristTVCell.swift
 //  Hotels
 //
-//  Created by Diyor Khalmukhamedov on 20/01/24.
+//  Created by Diyor Khalmukhamedov on 19/01/24.
 //
 
-import Foundation
+import UIKit
+
+class AddTouristTVCell: UITableViewCell {
+
+    static let id = "AddTouristTVCell"
+
+    // MARK: - UI elements
+    let topSeperatorView: UIView = {
+        let separator = UIView()
+        separator.backgroundColor = MainHotelColors.seperatorColor
+        return separator
+    }()
+    let mainView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.masksToBounds = true
+        view.layer.cornerRadius = 12
+//        view.backgroundColor = .green
+        return view
+    }()
+    let customerInfoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Добавить туриста"
+        label.font = label.font.withSize(22)
+        label.textColor = .black
+        return label
+    }()
+    let expandButton: UIButton = {
+        let button = UIButton()
+        button.layer.cornerRadius = 6
+        button.backgroundColor = MainHotelColors.blueColor
+        button.setImage(UIImage(named: "plus"), for: .normal)
+        return button
+    }()
+    let bottomSeperatorView: UIView = {
+        let separator = UIView()
+        separator.backgroundColor = MainHotelColors.seperatorColor
+        return separator
+    }()
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setupUI()
+    }
+
+    private func setupUI() {
+        addSubviews(topSeperatorView, mainView, bottomSeperatorView)
+        topSeperatorView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.left.right.equalToSuperview()
+            make.height.equalTo(10)
+        }
+        mainView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(8)
+            make.left.right.equalToSuperview()
+            make.bottom.equalToSuperview().offset(-6)
+        }
+        bottomSeperatorView.snp.makeConstraints { make in
+            make.bottom.equalToSuperview()
+            make.left.right.equalToSuperview()
+            make.height.equalTo(10)
+        }
+        bringSubviewToFront(mainView)
+        mainView.addSubviews(customerInfoLabel, expandButton)
+        customerInfoLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(16)
+            make.left.equalToSuperview().offset(16)
+        }
+        expandButton.snp.makeConstraints { make in
+            make.width.height.equalTo(32)
+            make.right.equalToSuperview().offset(-16)
+            make.top.equalToSuperview().offset(16)
+        }
+    }
+}
--- Hotels/Booking/Views/BookingViewController.swift
+++ Hotels/Booking/Views/BookingViewController.swift
@@ -10,24 +10,27 @@ import UIKit
 class BookingViewController: UIViewController {
     var customFirstCellHeight = 450.0
     var customSecondCellHeight = 72.0
-    var customThirdCellHeight = 72.0
+    var customCellHeight = 72.0
     var isfirstTouristCellExpanded = true
     var isSecondTouristCellExpanded = false
-    var isThirdTouristCellExpanded = false
-    var totalCellsCount = 7
-//    var totalTouristCount = 2
+    var isTouristCellExpanded = false
+    var totalCells = 7
     // MARK: - Coordinator
     var coordinator: HotelCoordinator?
     // MARK: - ViewModel || Model
     let viewModel = BookingViewModel()
     var hotelRoomModel: BookingModel?
     // MARK: - User Data
-    var name = ""
-    var surname = ""
-    var birthDate = ""
-    var citizenship = ""
-    var passportNumber = ""
-    var passportValidTill = ""
+    var dataSource = [
+        "HotelNameTVCell",
+        "BookingInfoTVCell",
+        "CustomerInfoTVCell",
+        "FirstTouristTVCell",
+        "SecondTouristTVCell",
+        "AddTouristTVCell",
+        "FooterTVCell"
+    ]
+    var isTouristCellExpandedArray: [Bool] = []
     // MARK: - UI Elements
     let mainTableView: UITableView = {
         let tableview = UITableView()
@@ -37,6 +40,7 @@ class BookingViewController: UIViewController {
         tableview.register(CustomerInfoTVCell.self, forCellReuseIdentifier: CustomerInfoTVCell.id)
         tableview.register(FirstTouristTVCell.self, forCellReuseIdentifier: FirstTouristTVCell.id)
         tableview.register(SecondTouristTVCell.self, forCellReuseIdentifier: SecondTouristTVCell.id)
+        tableview.register(NewTouristTVCell.self, forCellReuseIdentifier: NewTouristTVCell.id)
         tableview.register(AddTouristTVCell.self, forCellReuseIdentifier: AddTouristTVCell.id)
         tableview.register(TotalCostTVCell.self, forCellReuseIdentifier: TotalCostTVCell.id)
         tableview.separatorStyle = .none
@@ -109,20 +113,33 @@ class BookingViewController: UIViewController {
     }
     // MARK: - Actions
     @objc func mainButtonTapped() {
+        var allCellsValid = true
+        
         for section in 0..<mainTableView.numberOfSections {
             for row in 0..<mainTableView.numberOfRows(inSection: section) {
-                if let cell = mainTableView.cellForRow(at: IndexPath(row: row, section: section)) as? FirstTouristTVCell {
-                    // Check each text field in the cell
-                    if !cell.areTextFieldsValid() {
-                        // Show an alert since at least one text field is empty
+                if let firstCell = mainTableView.cellForRow(at: IndexPath(row: row, section: section)) as? FirstTouristTVCell {
+                    if !firstCell.areTextFieldsValid() {
                         showErrorAlert(message: "Заполните все поля")
-                        return
-                    } else {
-                        setupCustomBackButton()
-                        coordinator?.showPaidVC()
+                        allCellsValid = false
+                        //                        break
+                    }
+                } else if let secondCell = mainTableView.cellForRow(at: IndexPath(row: row, section: section)) as? SecondTouristTVCell {
+                    if !secondCell.areTextFieldsValid() {
+                        showErrorAlert(message: "Заполните все поля")
+                        allCellsValid = false
+                        break
                     }
                 }
             }
+            
+            if !allCellsValid {
+                break
+            }
+        }
+        
+        if allCellsValid {
+            setupCustomBackButton()
+            coordinator?.showPaidVC()
         }
     }
     @objc func firstTouristButtonTapped(_ sender: UIButton) {
@@ -146,80 +163,108 @@ class BookingViewController: UIViewController {
         }
     }
     @objc func addTouristButtonTapped(_ sender: UIButton) {
-        sender.backgroundColor = MainHotelColors.blueColor.withAlphaComponent(0.1)
-        customThirdCellHeight = isThirdTouristCellExpanded ? 450.0 : 72.0
+        print("add tourist button tapped")
+        totalCells += 1
+//        totalTourists += 1
+
+        dataSource.insert("NewTouristTVCell", at: dataSource.firstIndex(of: "AddTouristTVCell")!)
+        while isTouristCellExpandedArray.count <= dataSource.count {
+            isTouristCellExpandedArray.append(false)
+        }
+        mainTableView.reloadData()
+    }
+    @objc func newTouristButtonTapped(_ sender: UIButton) {
+        let index = sender.tag
+
+        guard index < isTouristCellExpandedArray.count else {
+            // Ensure the index is within the array bounds
+            return
+        }
+
+        isTouristCellExpandedArray[index].toggle()
+
         let buttonImages = [UIImage(named: "expandUP"), UIImage(named: "expandDOWN")]
-        sender.setImage(isThirdTouristCellExpanded ? buttonImages[0] : buttonImages[1], for: .normal)
-        isThirdTouristCellExpanded.toggle()
+        sender.setImage(isTouristCellExpandedArray[index] ? buttonImages[0] : buttonImages[1], for: .normal)
+        
+        customCellHeight = isTouristCellExpandedArray[index] ? 450.0 : 72.0
+
         UIView.animate(withDuration: 0.3) { [self] in
-            mainTableView.beginUpdates()
-            mainTableView.endUpdates()
+            self.mainTableView.beginUpdates()
+            self.mainTableView.endUpdates()
         }
     }
 }
 // MARK: - Tableview Delegate || DataSource
 extension BookingViewController: UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        let index = indexPath.row
-        switch index {
-        case 0:
+        let cellData = dataSource[indexPath.row]
+        switch cellData {
+        case "HotelNameTVCell":
             return 170
-        case 1:
+        case "BookingInfoTVCell":
             return 300
-        case 2:
+        case "CustomerInfoTVCell":
             return 252
-        case 3:
+        case "FirstTouristTVCell":
             return customFirstCellHeight
-        case 4:
+        case "SecondTouristTVCell":
             return customSecondCellHeight
-        case 5:
-            return customThirdCellHeight
-        case 6:
+        case "AddTouristTVCell":
+            return 72.0
+        case "FooterTVCell":
             return 160
         default:
-            return customSecondCellHeight
+            return customCellHeight
         }
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return totalCellsCount
+        return totalCells
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let index = indexPath.row
-        switch index {
-        case 0:
+        let cellData = dataSource[indexPath.row]
+        switch cellData {
+        case "HotelNameTVCell":
             let cell = tableView.dequeueReusableCell(withIdentifier: "HotelNameTVCell", for: indexPath) as! HotelNameTVCell
             cell.configure(with: hotelRoomModel)
             return cell
-        case 1:
+        case "BookingInfoTVCell":
             let cell = tableView.dequeueReusableCell(withIdentifier: "BookingInfoTVCell", for: indexPath) as! BookingInfoTVCell
             cell.configure(with: hotelRoomModel)
             return cell
-        case 2:
+        case "CustomerInfoTVCell":
             let cell = tableView.dequeueReusableCell(withIdentifier: "CustomerInfoTVCell", for: indexPath) as! CustomerInfoTVCell
             return cell
-        case 3:
+        case "FirstTouristTVCell":
             let cell = tableView.dequeueReusableCell(withIdentifier: "FirstTouristTVCell", for: indexPath) as! FirstTouristTVCell
             cell.expandButton.addTarget(self, action: #selector(firstTouristButtonTapped), for: .touchUpInside)
             return cell
-        case 4:
+        case "SecondTouristTVCell":
             let cell = tableView.dequeueReusableCell(withIdentifier: "SecondTouristTVCell", for: indexPath) as! SecondTouristTVCell
             cell.customerInfoLabel.text = "Второй турист"
             cell.expandButton.addTarget(self, action: #selector(secondTouristButtonTapped), for: .touchUpInside)
             return cell
-        case 5:
+        case "AddTouristTVCell":
             let cell = tableView.dequeueReusableCell(withIdentifier: "AddTouristTVCell", for: indexPath) as! AddTouristTVCell
-            cell.customerInfoLabel.text = "Добавить туриста"
-            cell.expandButton.backgroundColor = MainHotelColors.blueColor
-            cell.expandButton.setImage(UIImage(named: "plus"), for: .normal)
             cell.expandButton.addTarget(self, action: #selector(addTouristButtonTapped), for: .touchUpInside)
             return cell
-        case 6:
+        case "FooterTVCell":
             let cell = tableView.dequeueReusableCell(withIdentifier: "TotalCostTVCell", for: indexPath) as! TotalCostTVCell
             cell.configure(with: hotelRoomModel)
             return cell
         default:
-            let cell = tableView.dequeueReusableCell(withIdentifier: "SecondTouristTVCell", for: indexPath) as! SecondTouristTVCell
+            let cell = tableView.dequeueReusableCell(withIdentifier: "NewTouristTVCell", for: indexPath) as! NewTouristTVCell
+            cell.expandButton.tag = indexPath.row
+            let touristNumber = indexPath.row - 2
+            let ruNumber = russianOrdinalNumber(touristNumber)
+            print("ruNumber.row: \(ruNumber)")
+            cell.customerInfoLabel.text = "\(ruNumber) турист"
+            cell.expandButton.addTarget(self, action: #selector(newTouristButtonTapped), for: .touchUpInside)
             return cell
         }
     }
+    // Function to convert number to Russian ordinal number
+    func russianOrdinalNumber(_ number: Int) -> String {
+        return "\(number)-й"
+    }
 }
+
